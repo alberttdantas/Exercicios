7068ccccea3ddc07eb36b502e8fce1d313741ade
AlbertDantas <albert.oliveira@zukk.com.br>
2023-03-27 23:44:29 -0300

    Add código

---
+++ Exercicio_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SomaConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 100000; // número dado para calcular a soma
+            int soma = Soma(num);
+            Console.WriteLine($"A soma de 1 até {num} é: {soma}");
+        }
+
+        static int Soma(int num)
+        {
+            if (num < 1) // verifica se o número é maior que zero
+            {
+                throw new ArgumentException("O número deve ser maior que zero.");
+            }
+
+            int soma = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                soma += i;
+            }
+            return soma;
+        }
+    }
+}
---
+++ Exercicio_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DuplicadosConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavras = { "abracadabra", "allottee", "assessee", "kelless","keenness","Alfalggo" };
+            string[] resultado = Duplicados(palavras);
+            Console.WriteLine(string.Join(", ", resultado));
+        }
+
+        static string[] Duplicados(string[] palavras)
+        {
+            List<string> resultado = new List<string>();
+
+            foreach (string palavra in palavras)
+            {
+                char[] chars = palavra.ToCharArray();
+                string novaPalavra = "" + chars[0];
+
+                for (int i = 1; i < chars.Length; i++)
+                {
+                    if (chars[i] != chars[i - 1])
+                    {
+                        novaPalavra += chars[i];
+                    }
+                }
+
+                resultado.Add(novaPalavra);
+            }
+
+            return resultado.ToArray();
+        }
+    }
+}
